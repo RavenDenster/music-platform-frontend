df8413699775f4f597f9d9e5879935135e62f0bb
Oleg Kashechkin <95638297+RavenDenster@users.noreply.github.com>
2023-04-22 23:48:44 +0300

    path

--- http/index.ts
+++ http/index.ts
@@ -1,8 +1,9 @@
 import axios from 'axios'
 // https://music-platform-backend-delta.vercel.app/
 // http://localhost:5000/
+// https://music-platform-backend.onrender.com/
 
-export const SERVER_URL = `https://music-platform-backend-delta.vercel.app/`
+export const SERVER_URL = `https://music-platform-backend.onrender.com/`
 export const API_URL = `${SERVER_URL}auth`
 
 const $api = axios.create({
